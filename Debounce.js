ea82078ebb56deee0037fa5507940686b169eabe
Vikas Singh <44vikasrajput44@gmail.com>
2025-08-04 12:03:05 +0530

    Update Debounce.js

--- Debounce.js
+++ Debounce.js
@@ -10,3 +10,10 @@ export default function debounce(func, wait) {
 
   throw 'Not implemented!';
 }
+
+
+//main idea is  to use: a timer that resets every time a debounce function is called. 
+ //only when the timer completes without interrruption we can execute the callback.
+ //Use clearTimeout to clear previous scheduled timers.
+ //Use setTimeout to schedule a new execution after wait period.
+ // use func.apply( this, args) to ensure that callback runs with correct this and arguments.
